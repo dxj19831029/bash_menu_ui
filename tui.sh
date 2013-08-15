3e8bd8fb6b8a255119d8242eba7f5f77c7b017b4
xd <dxj19831029@gmail.com>
2013-08-15 17:58:43 +0800

    allow to have hidden area

--- tui.sh
+++ tui.sh
@@ -4,6 +4,11 @@ MENU_OPTIONS=()
 LM=0
 TITLE="BASH SELECTION MENU                        "
 TOP_LANE=1
+MAX_LINES=`tput lines`
+CURRENT_LANE=0
+SHOW_LINES=MAX_LINES
+TOTAL_LINES=0
+IS_DEBUG=${DEBUG:-no}
 
 
 #### modified based on http://top-scripts.blogspot.com/2011/01/blog-post.html
@@ -21,6 +26,10 @@ BLUE(){ $e ; };BLUE
 C(){ CLEAR;BLUE;}
 HEAD(){ 
 	TOTAL_BORDER=$(( $LM + 4 ))
+  if test "$IS_DEBUG" = "yes" ; then 
+    TPUT 0 0 
+    printf "total: $TOTAL_LINES show: $SHOW_LINES LM: $LM i: $i current top: $CURRENT_LANE l opt index: $L_OPT_INDEX"
+  fi
 	MARK;TPUT $TOP_LANE 4
 	EMPTY1="                         "
 	EMPTY2="                         "
@@ -37,22 +46,37 @@ ARROW(){ read -s -n3 key 2>/dev/null >&2
 	if [[ $key = $ESC[B ]];then echo dn;fi;}
 POSITION(){ if [[ $cur = up ]];then ((i--));fi
 	if [[ $cur = dn ]];then ((i++));fi
-	if [[ i -lt 0   ]];then i=$LM;fi
-	if [[ i -gt $LM ]];then i=0;fi;}
+  if [[ i -lt 0 ]]; then ((CURRENT_LANE--)); INITN;fi
+  if [[ i -gt LM ]]; then ((CURRENT_LANE++));INITN;fi
+	if [[ i -lt 0 && CURRENT_LANE -lt 0  ]];then i=$LM; CURRENT_LANE=$(( $TOTAL_LINES - $LM )); INITN;fi
+  VIEW_REGION=$(( $CURRENT_LANE + $SHOW_LINES ))
+	if [[ i -gt LM && VIEW_REGION -gt TOTAL_LINES ]];then i=0; CURRENT_LANE=0 ; INITN;fi;
+  if [[ i -gt LM ]] ; then i=$LM;INITN ; fi;
+  if [[ i -lt 0 ]] ; then i=0;INITN; fi;
+  #if test "$IS_DEBUG" = "yes" ; then 
+    #TPUT 0 0 
+    #printf "total: $TOTAL_LINES show: $SHOW_LINES LM: $LM i: $i current top: $CURRENT_LANE l opt index: $L_OPT_INDEX"
+  #fi
+}
 REFRESH(){ after=$((i+1)); before=$((i-1))
 	if [[ $before -lt 0  ]];then before=$LM;fi
 	if [[ $after -gt $LM ]];then after=0;fi
 	if [[ $j -lt $i      ]];then UNMARK;MN "$before";else UNMARK;MN "$after";fi
 	if [[ $after -eq 0   ]] || [[ $before -eq $LM ]];then
-	UNMARK; MN "$before"; MN "$after";fi;j=$i;UNMARK;MN "$before" ; MN "$after";}
-	MN(){ line=$(( $1  + 1 + $TOP_LANE )); 
+    UNMARK; MN "$before"; MN "$after";
+  fi;
+  j=$i;UNMARK;MN "$before" ; MN "$after";
+}
+MN(){ 
+    line=$(( $1  + 1 + $TOP_LANE )); 
 		TPUT "$line" 5 ; 
 		EMPTY1="                        "
 		EMPTY2="                       "
-		OUTPUT_OPTION_L="${MENU_OPTIONS[$1]:-None}" ;
+    L_OPT_INDEX=$(( $1 + $CURRENT_LANE ))
+		OUTPUT_OPTION_L="${MENU_OPTIONS[$L_OPT_INDEX]:-None}" ;
 		$e "${EMPTY1:$(( ${#OUTPUT_OPTION_L} / 2 ))} ${OUTPUT_OPTION_L} ${EMPTY2:$(( ${#OUTPUT_OPTION_L} / 2 ))}";
 		#$e "${MENU_OPTIONS[$1]:-None}" ; 
-	}
+}
 LM=6    #Last Menu number
 MENUN(){ 
 	N_L=0
@@ -74,13 +98,25 @@ COUNT_LM() {
 	fi
 }
 
+adjust_show_region()
+{
+  SHOW_LINES=$(( $MAX_LINES - $TOP_LANE - 5 ))
+  TOTAL_LINES=$LM
+  if [[ $LM -gt $SHOW_LINES ]] ; then
+    LM=$SHOW_LINES
+  fi
+}
+
 # more than two options will be handled correctly
 show_menu() {
 	i=0
+  CURRENT_LANE=0
+  MAX_LINES=`tput lines`
 	COUNT_LM
+  adjust_show_region
 	INITN
 	while [[ "$O" != " " ]] ; do
-		S="MN $i" ; SC; if [[ $cur = "" ]]; then C; TPUT 1 1 ; return $i;fi
+		S="MN $i" ; SC; if [[ $cur = "" ]]; then C; TPUT 1 1 ; ret_l=$(( $CURRENT_LANE + $i )); return $ret_l;fi
 		POSITION
 	done
 }
@@ -88,12 +124,15 @@ show_menu() {
 # more than two options will be handled correctly
 show_menu_quit() {
 	i=0
+  CURRENT_LANE=0
+  MAX_LINES=`tput lines`
 	COUNT_LM
 	LM=$(( $LM + 1 ))
 	MENU_OPTIONS[$LM]="exit"
+  adjust_show_region
 	INITN
 	while [[ "$O" != " " ]] ; do
-		S="MN $i" ; SC; if [[ $cur = "" ]]; then C; TPUT 1 1 ; if [[ "$i" = "$LM" ]] ; then exit 0; fi; return $i;fi
+		S="MN $i" ; SC; if [[ $cur = "" ]]; then C; TPUT 1 1 ; ret_l=$(( $CURRENT_LANE + $i )); if [[ "$ret_l" = "$LM" ]] ; then exit 0; fi; return $ret_l;fi
 		POSITION
 	done
 }
