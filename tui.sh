fb1cfc2ecb204a422499fcfef927ace838f2c549
xd <dxj19831029@gmail.com>
2013-08-22 15:15:35 +0800

    enable the multi-selected options

--- tui.sh
+++ tui.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 MENU_OPTIONS=()
+M_SELECTED=()
+M_SELECTED_INDEX=()
 LM=0
 TITLE="BASH SELECTION MENU                        "
 TOP_LANE=1
@@ -9,6 +11,7 @@ CURRENT_LANE=0
 SHOW_LINES=MAX_LINES
 TOTAL_LINES=0
 IS_DEBUG=${DEBUG:-no}
+IS_MULTI_SELECT=false
 
 
 #### modified based on http://top-scripts.blogspot.com/2011/01/blog-post.html
@@ -40,11 +43,39 @@ HEAD(){
 	done;WRITE;}
 i=0; CLEAR; CIVIS;NULL=/dev/null
 FOOT(){ MARK;end_foot=$(( $LM + 4 + $TOP_LANE )) ; TPUT $end_foot 4
-	printf "      ENTER=SELECT, UP/DN=NEXT OPTION               ";UNMARK;}
-ARROW(){ read -s -n3 key 2>/dev/null >&2
-	if [[ $key = $ESC[A ]];then echo up;fi
-	if [[ $key = $ESC[B ]];then echo dn;fi;}
-POSITION(){ if [[ $cur = up ]];then ((i--));fi
+  if test "$IS_MULTI_SELECT" != "false" ; then
+	  printf " ENTER=FINISH, UP/DN=NEXT OPTION, S=select/deselect ";UNMARK;
+  else
+	  printf "      ENTER=SELECT, UP/DN=NEXT OPTION               ";UNMARK;
+  fi
+  }
+ARROW(){ 
+  while true;  do
+    read -s -n1 key 2>/dev/null >&2
+    if [[ $key = 's' && "$IS_MULTI_SELECT" != "false" ]] ; then
+      echo select; break; 
+      #L_C=$(( $CURRENT_LANE + $i )) ; ${M_SELECTED[$L_C]} = true  ; INITN; 
+      continue;
+    fi
+    if [[ $key = '' ]] ; then break; fi
+    if test "$key" != "$ESC" ; then continue; fi
+    read -s -n2 key2 2>/dev/null >&2
+	  if [[ $key2 = [A ]];then echo up; break; fi
+	  if [[ $key2 = [B ]];then echo dn; break; fi;
+  done
+  }
+POSITION(){ 
+  if [[ $cur = select ]]; then 
+    L_C=$(( $CURRENT_LANE + $i )) ; 
+    if test "${M_SELECTED[$L_C]}" = "true" ; then
+      M_SELECTED[$L_C]=""
+    else
+      M_SELECTED[$L_C]=true  
+    fi
+    INITN
+     return 0
+  fi
+  if [[ $cur = up ]];then ((i--));fi
 	if [[ $cur = dn ]];then ((i++));fi
   if [[ i -lt 0 ]]; then ((CURRENT_LANE--)); INITN;fi
   if [[ i -gt LM ]]; then ((CURRENT_LANE++));INITN;fi
@@ -70,11 +101,26 @@ REFRESH(){ after=$((i+1)); before=$((i-1))
 MN(){ 
     line=$(( $1  + 1 + $TOP_LANE )); 
 		TPUT "$line" 5 ; 
-		EMPTY1="                        "
-		EMPTY2="                       "
     L_OPT_INDEX=$(( $1 + $CURRENT_LANE ))
-		OUTPUT_OPTION_L="${MENU_OPTIONS[$L_OPT_INDEX]:-None}" ;
-		$e "${EMPTY1:$(( ${#OUTPUT_OPTION_L} / 2 ))} ${OUTPUT_OPTION_L} ${EMPTY2:$(( ${#OUTPUT_OPTION_L} / 2 ))}";
+    l_select=${M_SELECTED[$L_OPT_INDEX]}
+    empty_str=" "
+
+    if test "$IS_MULTI_SELECT" != "false" ; then
+      if test "$l_select" = "true" ; then 
+        empty_str=" [x]" ; 
+      else 
+        empty_str=" [ ]"; 
+      fi
+		  EMPTY1="                      "
+		  EMPTY2="                     "
+		  OUTPUT_OPTION_L="${MENU_OPTIONS[$L_OPT_INDEX]:-None}" ;
+		  $e "${empty_str}${EMPTY1:$(( ${#OUTPUT_OPTION_L} / 2 ))} ${OUTPUT_OPTION_L} ${EMPTY2:$(( ${#OUTPUT_OPTION_L} / 2 ))}";
+    else
+		  EMPTY1="                        "
+		  EMPTY2="                       "
+		  OUTPUT_OPTION_L="${MENU_OPTIONS[$L_OPT_INDEX]:-None}" ;
+		  $e "${EMPTY1:$(( ${#OUTPUT_OPTION_L} / 2 ))} ${OUTPUT_OPTION_L} ${EMPTY2:$(( ${#OUTPUT_OPTION_L} / 2 ))}";
+    fi
 		#$e "${MENU_OPTIONS[$1]:-None}" ; 
 }
 LM=6    #Last Menu number
@@ -107,16 +153,32 @@ adjust_show_region()
   fi
 }
 
+collect_multi_select_index()
+{
+  if test "$IS_MULTI_SELECT" != "false" ; then
+    N=0
+    N_INDEX=0
+    for option in ${MENU_OPTIONS[@]} ; do
+      if test "${M_SELECTED[$N]}" = "true" ; then
+        M_SELECTED_INDEX[$N_INDEX]=$N
+        (( N_INDEX++ ))
+      fi
+      (( N++ ))
+    done
+  fi
+}
+
 # more than two options will be handled correctly
 show_menu() {
 	i=0
   CURRENT_LANE=0
+  M_SELECTED=()
   MAX_LINES=`tput lines`
 	COUNT_LM
   adjust_show_region
 	INITN
 	while [[ "$O" != " " ]] ; do
-		S="MN $i" ; SC; if [[ $cur = "" ]]; then C; TPUT 1 1 ; ret_l=$(( $CURRENT_LANE + $i )); return $ret_l;fi
+		S="MN $i" ; SC; if [[ $cur = "" ]]; then C; TPUT 1 1 ; ret_l=$(( $CURRENT_LANE + $i )); collect_multi_select_index; return $ret_l;fi
 		POSITION
 	done
 }
@@ -125,6 +187,7 @@ show_menu() {
 show_menu_quit() {
 	i=0
   CURRENT_LANE=0
+  M_SELECTED=()
   MAX_LINES=`tput lines`
 	COUNT_LM
 	LM=$(( $LM + 1 ))
@@ -132,7 +195,7 @@ show_menu_quit() {
   adjust_show_region
 	INITN
 	while [[ "$O" != " " ]] ; do
-		S="MN $i" ; SC; if [[ $cur = "" ]]; then C; TPUT 1 1 ; ret_l=$(( $CURRENT_LANE + $i )); if [[ "$ret_l" = "$LM" ]] ; then exit 0; fi; return $ret_l;fi
+		S="MN $i" ; SC; if [[ $cur = "" ]]; then C; TPUT 1 1 ; ret_l=$(( $CURRENT_LANE + $i )); if [[ "$ret_l" = "$LM" ]] ; then exit 0; fi; collect_multi_select_index ; return $ret_l;fi
 		POSITION
 	done
 }
@@ -152,8 +215,9 @@ TITLE="multiple selection"
 show_menu
 echo "selection: $?"
 
+IS_MULTI_SELECT=true
 MENU_OPTIONS=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 )
 TITLE="multiple selection"
 show_menu_quit
-echo "selection: $?"
+echo "selection: $? multi selected: ${M_SELECTED_INDEX[@]}, all: ${M_SELECTED[@]}"
 
